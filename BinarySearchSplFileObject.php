8142223938737b6b7e4900e1fa0cd702f7d77313
stasya0903 <stasya0903@mail.ru>
2020-01-30 12:42:27 -0500

    added comments

--- BinarySearchSplFileObject.php
+++ BinarySearchSplFileObject.php
@@ -1,55 +1,74 @@
 <?php
+/**
+ * Функция нахождения размера файла размером больше 2 ГБ
+ * Источник php.net/manual/ru/function.filesize.php
+ * @param $file
+ * @return false|int|mixed
+ */
 function find_filesize($file)
 {
-    if(substr(PHP_OS, 0, 3) == "WIN")
-    {
-        exec('for %I in ("'.$file.'") do @echo %~zI', $output);
+    if (substr(PHP_OS, 0, 3) == "WIN") {
+        exec('for %I in ("' . $file . '") do @echo %~zI', $output);
         $return = $output[0];
-    }
-    else
-    {
+    } else {
         $return = filesize($file);
     }
     return $return;
 }
 
-function binarySearch ($file, $key){
+/**
+ * @param $file
+ * @param $key
+ * @return mixed|string
+ */
 
+function binarySearch($file, $key)
+{
+    // создаем обьект файла используя библеотеку SPL
     $fileObj = new SplFileObject($file);
+    // инициализируем счетчик итераций
     $n = 0;
+    // ставим левую границу  поиска в начало файла
     $left = 0;
+    //определяем размер файла
     $fileSize = find_filesize($file);
+    // переносим указатель в конец файла
     $fileObj->seek($fileSize);
+    // получаем значение коненой строки
     $right = $fileObj->key();
+    // ищем по файлу пока правая граница поиска не выходит за левую
     while ($left <= $right) {
+        // на каждом проходе прибавляем кол-во инициализаций
         $n++;
-       $middle = floor(($left+$right)/2);
-
-
-        echo "Проверяется элемент с индексом: $middle".  "</br>";
-
+        // вычесляем среднее значение
+        $middle = floor(($left + $right) / 2);
+        echo "Проверяется элемент с индексом: $middle" . "</br>";
+        // переводим индекс в среднее значение
         $fileObj->seek($middle);
+        // создаем массив из текущего значения
         $currentElement = explode("\t", $fileObj->current());
-
-        if (strnatcmp($currentElement[0],$key) == 0) {
-            echo "КОЛИЧЕСТВО ИТЕРАЦИЙ: ". $n .  "</br>";
+        // сравниваем с помощью алгоритма "natural order"
+        // если ключ совпадает выходим из цикла и возвращаем значение
+        if (strnatcmp($currentElement[0], $key) == 0) {
+            echo "КОЛИЧЕСТВО ИТЕРАЦИЙ: " . $n . "</br>";
             return $currentElement[1];
-        }
-
-        elseif (strnatcmp($currentElement[0],$key) > 0) {
+            // в случае если функция strnatcmp возвращает отриц значение ключ находиться выше текущего значения
+        } elseif (strnatcmp($currentElement[0], $key) > 0) {
+            //чтобы продолжить поиск в левой половине перемещаем правую границу в положение середины
             $right = $middle - 1;
-        }
-
-        elseif (strnatcmp($currentElement[0],$key) < 0) {
+            // в случае если функция strnatcmp возвращает полож  значение ключ находиться  ниже текущего значения
+        } elseif (strnatcmp($currentElement[0], $key) < 0) {
+            //чтобы продолжить поиск в правой половине перемещаем левую  границу в положение середины
             $left = $middle + 1;
         }
 
     }
 
-    echo "КОЛИЧЕСТВО ИТЕРАЦИЙ: ".$n. "</br>";
+    echo "КОЛИЧЕСТВО ИТЕРАЦИЙ: " . $n . "</br>";
     return "undef";
 
 }
+
 $start = microtime(true);
 echo binarySearch("test.txt", "ключ1000000");
-echo   microtime(true) - $start;
+echo microtime(true) - $start;
